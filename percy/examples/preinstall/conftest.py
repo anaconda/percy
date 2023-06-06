811aed7b1494e0cd76cc85c8925cbde01b6070a0
Carl Anderson (barabo) <canderson@anaconda.com>
2023-06-06 17:05:31 -0500

    update tests to use the local recipe

--- percy/examples/preinstall/conftest.py
+++ percy/examples/preinstall/conftest.py
@@ -1,5 +1,5 @@
 _DEFAULT_AGGREGATE = "~/work/recipes/aggregate/"
-_DEFAULT_PYTHON = ['3.7', '3.8', '3.9', '3.10']
+_DEFAULT_PYTHON = ['3.8', '3.9', '3.10', '3.11']
 _DEFAULT_SUBDIRS = [
     'linux-64',
     'linux-ppc64le',
