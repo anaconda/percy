a047b6386332c39a30f2967e15e01895f142b114
Charles Bousseau <cbousseau@anaconda.com>
2023-06-07 17:40:55 -0400

    refine applying patches

--- percy/examples/updater.py
+++ percy/examples/updater.py
@@ -1,4 +1,8 @@
 """ Update a recipe
+
+- Increment build number to latest+1 from what is on main.
+- Pass a dictionary of dependencies to add or update.
+
 """
 
 import percy.render.recipe as recipe
@@ -6,6 +10,7 @@ import argparse
 from pathlib import Path
 import logging
 import re
+import copy
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
@@ -22,25 +27,38 @@ def create_parser() -> argparse.ArgumentParser:
         "--recipe_path",
         type=Path,
         help="recipe path",
-        default="/Users/cbousseau/work/recipes/aggregate2/mamba-feedstock/recipe/meta.yaml",
+        default="/Users/cbousseau/work/recipes/aggregate2/cartopy-feedstock/recipe/meta.yaml",
     )
     return parser
 
-def update_dep(package, section, pkg, constraint):
-    if package.has_dep(section, pkg):
-        _replace_dep(package.recipe, f"{package.path_prefix}requirements/{section}", pkg, constraint)
-    else:
-        _add_dep(package.recipe, f"{package.path_prefix}requirements/{section}", pkg, constraint) 
+def update_dep(operation, package, section, pkg, constraints):
+    
+    # get initial section range
+    recipe = package.recipe
+    (start_row, start_col, end_row, end_col) = recipe.get_raw_range(f"{package.path_prefix}requirements/{section}")
+    range = copy.deepcopy(recipe.meta_yaml[start_row:end_row])
 
-def _replace_dep(recipe, path, pkg, constraint):
-    (start_row, start_col, end_row, end_col) = recipe.get_raw_range(path)
-    sub_text = "\n".join(recipe.meta_yaml[start_row:end_row])
-    sub_text = re.sub(f'(?=\s*?)-\s+{pkg}\s*.*', f'- {pkg} {constraint}', sub_text)
-    recipe.meta_yaml[start_row:end_row] = sub_text.split("\n")
+    # does section has dep?
+    has_dep = package.has_dep(section, pkg)
 
-def _add_dep(recipe, path, pkg, constraint):
-    (_, start_col, end_row, _) = recipe.get_raw_range(path)
-    recipe.meta_yaml.insert(end_row, " " * start_col + f"- {pkg} {constraint}")
+    # remove elements
+    if operation in ["remove", "replace", "add_or_replace"]:
+        range = [l for l in range if not pkg in l ]
+
+    # add elements
+    if operation in ["add", "replace", "add_or_replace"]:
+        if operation != "replace" or has_dep:
+            for constraint in constraints:
+                range.insert(end_row, " " * start_col + f"- {pkg} {constraint}")
+
+    # apply change
+    recipe.meta_yaml[start_row:end_row] = range
+
+def load_recipe(recipe_path):
+    others = {"r_implementation": "r-base"}
+    rendered_recipes = recipe.render(recipe_path, ["linux-64"], ["3.10"], others, False, recipe.RendererType.RUAMEL)
+    rendered_recipe = next(iter(rendered_recipes))
+    return rendered_recipe
 
 def increment_build_number(recipe):
     build_number = int(recipe.meta["build"]["number"]) + 1
@@ -61,35 +79,33 @@ if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
 
-    subdirs = [
-        # "osx-arm64",
-        # "osx-64",
-        # "win-64",
-        "linux-64",
-        # "linux-aarch64",
-        # "linux-ppc64le",
-        # "linux-s390x",
-    ]
+    rendered_recipe = load_recipe(args.recipe_path)
 
-    python = [
-        # "3.8",
-        # "3.9",
-        "3.10",
-        # "3.11",
-    ]
-
-    # load recipe
-    others = {"r_implementation": "r-base"}
-    rendered_recipes = recipe.render(args.recipe_path, subdirs, python, others, False, recipe.RendererType.RUAMEL)
-    rendered_recipe = next(iter(rendered_recipes))
 
     # update pin and increment build number
-    host_pkg = "openssl"
-    host_ver = "{{ openssl }}"
-    run_pkg = "openssl"
-    run_ver = "3.*"
-    for pkg_name, pkg in rendered_recipe.packages.items():
-        update_dep(pkg, "host", host_pkg, host_ver)
-        update_dep(pkg, "run", run_pkg, run_ver)
+    """
+        Patch operations:
+            replace : if package is found in section, replace by "- package constraint"
+            add: add "- package constraint" to section
+            add_or_replace : add or replace by "- package constraint"
+            remove: remove "- package" from section
+    """
+    patch = [
+        { "op": "replace", "section": "host", "package": "openssl", "constraint": ["{{ openssl }}"] },
+        { "op": "replace", "section": "run", "package": "openssl", "constraint": ["3.*"] },
+        { "op": "replace", "section": "host", "package": "numpy", "constraint": ["1.21  # [py<311]", "1.23  # [py>=311]"] },
+        { "op": "replace", "section": "run", "package": "numpy", "constraint": [">=1.21,<=2.0a0  # [py<311]", ">=1.23,<=2.0a0  # [py>=311]"] },
+        { "op": "remove", "section": "host", "package": "geos", "constraint": [] },
+        { "op": "remove", "section": "run", "package": "geos", "constraint": [] },
+        { "op": "add", "section": "host", "package": "geos2", "constraint": ["1.2.3"] },
+        { "op": "add", "section": "run", "package": "{{ pin_compatible('geos2') }}", "constraint": [""] },
+    ]
+    rendered_recipe = load_recipe(args.recipe_path)
     increment_build_number(rendered_recipe)
     rendered_recipe.save()
+    package_names = rendered_recipe.packages.keys()
+    for op in patch:
+        for package_name in package_names:
+            rendered_recipe = load_recipe(args.recipe_path)
+            update_dep(op["op"], rendered_recipe.packages[package_name], op["section"], op["package"], op["constraint"])
+            rendered_recipe.save()
